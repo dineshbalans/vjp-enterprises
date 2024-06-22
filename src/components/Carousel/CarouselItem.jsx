bbecc40420a6b2e878fc02e7c0fe606241d91f4e
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-06-22 15:27:27 +0530

    update: banner

--- src/components/Carousel/CarouselItem.jsx
+++ src/components/Carousel/CarouselItem.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { m } from "framer-motion";
 
 const CarouselItem = ({ URL }) => {
   return (
-    <li className=" w-full">
+    <li className="w-full relative">
       <img
         src={URL}
         alt=""
         className="m-auto max-h-full w-full max-w-full object-cover object-center"
       />
+      <div className="absolute inset-0 flex items-center p-28">
+        <m.h1
+          className="text-white text-7xl font-bold  w-1/2 leading-tight"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{delay: 0.3}}
+        >
+          "Breathe Fresh: Transform Your Space Instantly"
+        </m.h1>
+      </div>
     </li>
   );
 };
--- src/data/componentsData/carouselData.js
+++ src/data/componentsData/carouselData.js
@@ -1,30 +1,23 @@
-import image from "../../assets/heroBanner/image.png";
-import image1 from "../../assets/heroBanner/image (1).png";
-import image2 from "../../assets/heroBanner/image (2).png";
-import image3 from "../../assets/heroBanner/image (3).png";
-import image4 from "../../assets/heroBanner/image (4).png";
-import image5 from "../../assets/heroBanner/image (5).png";
-import image6 from "../../assets/heroBanner/image (6).png";
-import image7 from "../../assets/heroBanner/image (7).jpg";
+import banner1 from "../../assets/heroBanner/banner1.jpg";
+import banner2 from "../../assets/heroBanner/banner2.jpg";
 import image8 from "../../assets/heroBanner/image (8).png";
 import image9 from "../../assets/heroBanner/image (9).png";
-
 
 export const carouselData = [
   {
     id: "cd_1",
     // URL: "https://Tailwindmix.b-cdn.net/carousel/carousel-image-01.jpg",
-    URL: image8,
+    URL: banner1,
   },
   {
     id: "cd_2",
     // URL: "https://Tailwindmix.b-cdn.net/carousel/carousel-image-02.jpg",
-    URL: image9,
+    URL: banner2,
   },
-  {
-    id: "cd_3",
-    URL: "https://Tailwindmix.b-cdn.net/carousel/carousel-image-03.jpg",
-  },
+  // {
+  //   id: "cd_3",
+  //   URL: "https://Tailwindmix.b-cdn.net/carousel/carousel-image-03.jpg",
+  // },
   // {
   //   id: "cd_4",
   //   URL: "https://Tailwindmix.b-cdn.net/carousel/carousel-image-04.jpg",
