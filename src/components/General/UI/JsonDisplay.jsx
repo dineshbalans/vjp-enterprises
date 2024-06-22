90bf8740a88357269ba8263bdf093948a3c37e87
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-06-22 11:57:34 +0530

    update: product-details highlights

--- src/components/General/UI/JsonDisplay.jsx
+++ src/components/General/UI/JsonDisplay.jsx
@@ -4,11 +4,11 @@ const JsonDisplay = ({ data }) => {
   }
   return (
     <div className="space-y-3">
-      <h2 className="text-base md:text-lg font-medium">Highlights</h2>
+      <h2 className="text-base md:text-lg font-semibold underline">Product Info</h2>
       <ul className="list-disc">
         {Object.entries(JSON.parse(data)).map(([key, value], index) => (
           <li key={index} className=" flex gap-3">
-            <h6 className="w-1/3 truncate">{key}</h6>
+            <h6 className="w-1/3 truncate font-semibold">{key}</h6>
             <h6 className="w-1/2">{value}</h6>
             {/* <strong>{key}:</strong> {value} */}
           </li>
